08391509f2465f35d94e7ccae869d94105fac573
Gituser143 <bhargavsnv100@gmail.com>
2021-08-16 13:49:13 +0530

    Add license_check script

---
+++ scripts/license_check.go
@@ -0,0 +1,77 @@
+/*
+Copyright © 2021 Bhargav SNV bhargavsnv100@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const license = `/*
+Copyright © 2021 Bhargav SNV bhargavsnv100@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/`
+
+func main() {
+	verified := true
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if !info.IsDir() && strings.HasSuffix(path, ".go") {
+				bs, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
+
+				stringData := string(bs)
+				if !strings.HasPrefix(stringData, license) {
+					verified = false
+					log.Println("License not verified in", path)
+				}
+			}
+
+			return nil
+		})
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	if !verified {
+		os.Exit(1)
+	} else {
+		fmt.Println("All .go files have license verified.")
+	}
+}
